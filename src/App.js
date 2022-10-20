8cba4a5e14cae7907340cb71226ce70ef8e8e016
Zayne <star1582@naver.com>
2022-10-20 22:52:06 +0900

    day2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,45 @@ import React, { Component } from "react";
 import "./App.css";
 
 export default class App extends Component {
+  state = {
+    todoDate: [
+      {
+        id: '1',
+        title: '공부하기',
+        completed: true
+      },
+      {
+        id: '2',
+        title: '청소하기',
+        completed: false
+      }
+    ]
+  }
+
+  btnStyle = {
+    color: '#fff',
+    border: 'none',
+    padding: '5px 9px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
+  }
+
+  getStyle = () => {
+    return {
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: 'none'
+    }
+  }
+
+
+
+  handleClick = (id) => {
+    let newTodoData = this.state.todoDate.filter(data => data.id !== id);
+    this.setState({ todoDate: newTodoData });
+  };
+
   render() {
     return (
       <div className="container">
@@ -9,6 +48,14 @@ export default class App extends Component {
           <div className="title">
             <h1>할일 목록</h1>
           </div>
+          {this.state.todoDate.map((data) =>
+            <div style={this.getStyle()} key={data.id}>
+              <input type="checkbox" defaultChecked={false} />
+              {data.title}
+              <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>x</button>
+            </div>
+          )}
+
         </div>
       </div>
     )
